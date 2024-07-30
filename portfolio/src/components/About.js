84154e46ee5bdaca195ddabbe76798689b2f5153
Bryan Gonsalves <bryangonsalves01@gmail.com>
2024-07-30 18:03:18 +0530

    Added summary changes and an extra project

--- portfolio/src/components/About.js
+++ portfolio/src/components/About.js
@@ -4,11 +4,12 @@ import IMG from '../assets/nobg1.png';
 const About = () => {
     return (
         <div id="about" className="about">
+        
             <h1 className="about-heading">About Me</h1>
             <div className="about-info">
-                <p className="about-desc">Hi there! I'm a 2nd Computer Science student at Vellore institute of Technology with a drive to build software that has genuine positive impacts to the world. I'm constantly upskilling myself by working with new technology, and my favourites include React (ReactJs) for frontend full-stack development, and Python for backend and general problem solving.<br/><br/>
+                <p className="about-desc">Hi there! I'm a Final year Computer Science student at Vellore institute of Technology with a drive to build software that has genuine positive impacts to the world. I'm constantly upskilling myself by working with new technology, and my favourites include React (ReactJs) for frontend full-stack development, and Python for backend and general problem solving.<br/><br/>
 
-Beyond my technical endeavours, I'm also deeply interested in entrepreneurship, always exploring avenues to innovate and create solutions that address real-world challenges.</p>
+                In addition to my technical pursuits, I have a strong passion for entrepreneurship and innovation. I am constantly seeking opportunities to innovate and develop solutions that tackle real-world problems.</p>
                 <div className="about-img">
                     <div className="about-img-wrapper">
                         <img src={IMG} alt="Detective" />
